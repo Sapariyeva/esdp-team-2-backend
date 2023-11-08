41b3fbf8c57b1c39724ca986037890ed676d4e8f
Тургумбаев Наиль Рашитович <nailsanti2k19@gmail.com>
2023-11-08 20:29:51 +0600

    #17 deleted a field due to an eslint conflict

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,6 @@ module.exports = {
     'no-console': 'warn',
     'no-var': 'error',
     semi: 'error',
-    indent: ['error', 2, { SwitchCase: 1 }],
     'no-multi-spaces': 'error',
     'space-in-parens': 'error',
     'no-multiple-empty-lines': 'error',
