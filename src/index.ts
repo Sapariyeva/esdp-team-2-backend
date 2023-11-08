9bc39226365accc38339810425ff93925786237c
Тургумбаев Наиль Рашитович <nailsanti2k19@gmail.com>
2023-11-08 20:22:48 +0600

    #17 initCustomLocals use

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,14 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import 'reflect-metadata';
 import App from './app';
-import logger from './middlewares/logger';
+import logger from './middlewares/logger.middleware';
 import { ArticleRoute } from './routes/article.route';
 import { AuthRouter } from './routes/auth.route';
+import initCustomLocals from './middlewares/initCustomLocals.middleware';
 
 const app = new App({
   port: 8000,
-  middlewares: [logger(), cookieParser(), cors()],
+  middlewares: [initCustomLocals(), logger(), cookieParser(), cors()],
   controllers: [new ArticleRoute(), new AuthRouter()],
 });
 
