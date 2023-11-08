074fb12be87cb8def5f07d67f34ce578e37badc9
Тургумбаев Наиль Рашитович <nailsanti2k19@gmail.com>
2023-11-08 21:03:16 +0600

    #17 errors handler app

--- src/middlewares/errorsHandler.middleware.ts
+++ src/middlewares/errorsHandler.middleware.ts
@@ -1,10 +1,8 @@
-import { Request, Response } from 'express';
+import { ErrorRequestHandler } from 'express';
 import { ApiError } from '../helpers/api-error';
-
-export const errorsHandler = (err: Error, req: Request, res: Response) => {
+export const errorsHandler: ErrorRequestHandler = (err, req, res, next) => {
   if (err instanceof ApiError) {
     res.status(err.status).json({ message: err.message, errors: err.errors });
   }
-
   return res.status(500).json({ message: 'Internal Server Error', errors: [] });
 };
