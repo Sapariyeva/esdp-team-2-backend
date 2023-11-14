bfd19d728f490ae0072dcbf482f11be17e8a0545
Dmitry <mikotsowl22@gmail.com>
2023-11-14 19:36:34 +0600

    #36 Update patient entity

--- src/entities/patient.entity.ts
+++ src/entities/patient.entity.ts
@@ -1,37 +1,19 @@
-import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
-import bcrypt from 'bcrypt';
+import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { IPatient } from '../interfaces/IPatient.interface';
+import { User } from './user.entity';
 
 @Entity('patients')
 export class Patient implements IPatient {
   @PrimaryGeneratedColumn()
   id!: number;
 
-  @Column({ nullable: true, unique: true })
-  email!: string;
-
-  @Column({ nullable: true, unique: true })
-  phone!: string;
-
-  @Column()
-  password!: string;
-
-  @Column({ default: 'patient' })
-  role!: 'patient';
-
   @Column({ nullable: true })
   name!: string;
 
-  @BeforeInsert()
-  async hashPassword() {
-    const SALT_WORK_FACTOR = 10;
-    if (this.password) {
-      const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
-      this.password = await bcrypt.hash(this.password, salt);
-    }
-  }
+  @Column({ name: 'user_id' })
+  userId!: number;
 
-  async comparePassword(password: string): Promise<boolean> {
-    return await bcrypt.compare(password, this.password);
-  }
+  @OneToOne(() => User, (user) => user.patient)
+  @JoinColumn({ name: 'user_id' })
+  user?: User;
 }
