131adf17ca144a998351909951f224ac241cd22d
Тургумбаев Наиль Рашитович <nailsanti2k19@gmail.com>
2023-11-16 10:32:36 +0600

    #39 added the roles

--- src/db/seeds/main.seeds.ts
+++ src/db/seeds/main.seeds.ts
@@ -1,20 +1,13 @@
 import { DataSource } from 'typeorm';
 import { Seeder, SeederFactoryManager } from 'typeorm-extension';
-import { Patient } from '../../entities/patient.entity';
-import { PatientToken } from '../../entities/patientToken.entity';
+import { Role } from '../../entities/role.entity';
+import { UserRole } from '../../interfaces/UserRole.enum';
 
 export default class MainSeeder implements Seeder {
   public async run(dataSource: DataSource, factoryManager: SeederFactoryManager): Promise<void> {
-    const userFactory = factoryManager.get(Patient);
-    const tokenFactory = factoryManager.get(PatientToken);
+    const roleFactory = factoryManager.get(Role);
 
-    const users = await userFactory.saveMany(2);
-
-    await Promise.all(
-      users.map(async (patient) => {
-        await tokenFactory.saveMany(1, { patient: patient });
-        return;
-      }),
-    );
+    await roleFactory.save({ name: UserRole.Patient });
+    await roleFactory.save({ name: UserRole.Psychologist });
   }
 }
