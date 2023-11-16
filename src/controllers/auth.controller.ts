736d6e56481b5383faa118b0c43e3c6fa3e61aed
Тургумбаев Наиль Рашитович <nailsanti2k19@gmail.com>
2023-11-16 10:36:23 +0600

    #39 Added processing of user authentication operations

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,14 +1,12 @@
-import { RequestHandler } from 'express';
+import { RequestHandler, Response } from 'express';
 import { AuthService } from '../services/auth.service';
 import { plainToInstance } from 'class-transformer';
-import { SignInPatientDto } from '../dto/signInPatient.dto';
-import { SignUpPatientDto } from '../dto/signUpPatient.dto';
-import { PatientDto } from '../dto/patient.dto';
-import { Response } from 'express';
-import { validate } from 'class-validator';
+import { AuthUserDto } from '../dto/authUser.dto';
 import { ApiError } from '../helpers/api-error';
+import { validate } from 'class-validator';
 import { formatErrors } from '../helpers/formatErrors';
-import { IPatient, IPatientTokenData } from '../interfaces/IPatient.interface';
+import { IUser } from '../interfaces/IUser.interface';
+import { UserDto } from '../dto/user.dto';
 
 export class AuthController {
   private service: AuthService;
@@ -19,46 +17,34 @@ export class AuthController {
 
   signUp: RequestHandler = async (req, res, next) => {
     try {
-      const signUpPatientDto = plainToInstance(SignUpPatientDto, req.body);
-      await this.validateDto(signUpPatientDto);
+      const userDto = plainToInstance(AuthUserDto, req.body);
+      await this.validateDto(userDto);
 
-      const patientData = await this.service.signUp(signUpPatientDto);
-      if (!patientData) throw ApiError.BadRequest('Пользователь с такими данными уже зарегистрирован');
+      const userData = await this.service.signUp(userDto);
+      if (!userData) throw ApiError.BadRequest('s');
 
-      this.setRefreshTokenCookie(res, patientData.refreshToken);
-      res.send(this.mapUserDataToUserDto(patientData));
+      this.setRefreshTokenCookie(res, userData.refreshToken);
+      const user = this.mapUserDataToUserDto(userData);
+
+      res.send(user);
     } catch (e) {
       next(e);
     }
   };
-  refresh: RequestHandler = async (req, res, next) => {
-    try {
-      const patientTokenData = req.customLocals.patientTokenData;
-
-      if (!patientTokenData) throw ApiError.UnauthorizedError();
-
-      const refreshedPatientData = await this.service.refresh(patientTokenData);
-      this.handleSuccessfulRefresh(res, refreshedPatientData);
-    } catch (e) {
-      next(e);
-    }
-  };
-
   signIn: RequestHandler = async (req, res, next) => {
     try {
-      const signInUserDto = plainToInstance(SignInPatientDto, req.body);
-      console.log(await this.validateDto(signInUserDto));
-      const patient = await this.service.signIn(signInUserDto);
-      if (!patient) throw ApiError.BadRequest('Пользователь с такими данными не найден!');
+      const userDto = plainToInstance(AuthUserDto, req.body);
+      await this.validateDto(userDto);
+      const user = await this.service.signIn(userDto);
+      if (!user) throw ApiError.BadRequest('Пользователь с такими данными не найден!');
 
-      this.setRefreshTokenCookie(res, patient.refreshToken);
-      const userData = this.mapUserDataToUserDto(patient);
+      this.setRefreshTokenCookie(res, user.refreshToken);
+      const userData = this.mapUserDataToUserDto(user);
       res.send(userData);
     } catch (e) {
       next(e);
     }
   };
-
   signOut: RequestHandler = async (req, res, next) => {
     try {
       const { refreshToken } = req.cookies;
@@ -70,23 +56,34 @@ export class AuthController {
     }
   };
 
+  refresh: RequestHandler = async (req, res, next) => {
+    try {
+      const userTokenData = req.customLocals.patientTokenData;
+
+      if (!userTokenData) throw ApiError.UnauthorizedError();
+      const refreshedUserData = await this.service.refresh(userTokenData);
+
+      if (!refreshedUserData) throw ApiError.BadRequest('Пользователь с такими данными не найден!');
+
+      this.setRefreshTokenCookie(res, refreshedUserData.refreshToken);
+      res.send(this.mapUserDataToUserDto(refreshedUserData));
+    } catch (e) {
+      next(e);
+    }
+  };
+
   private setRefreshTokenCookie(res: Response, refreshToken: string): void {
     res.cookie('refreshToken', refreshToken, { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true });
   }
 
-  private mapUserDataToUserDto(patientData: IPatient): PatientDto {
-    return plainToInstance(PatientDto, patientData, { excludeExtraneousValues: true });
+  private mapUserDataToUserDto(userData: IUser) {
+    return plainToInstance(UserDto, userData, { excludeExtraneousValues: true });
   }
-  private async validateDto(dto: SignUpPatientDto | SignInPatientDto) {
+  private async validateDto(dto: AuthUserDto) {
     const errors = await validate(dto, {
       whitelist: true,
       validationError: { target: false, value: false },
     });
     if (errors.length) throw ApiError.BadRequest('Ошибка при валидации формы', formatErrors(errors));
-  }
-  private handleSuccessfulRefresh(res: Response, refreshedUserData: IPatientTokenData | null) {
-    if (!refreshedUserData) throw ApiError.BadRequest('User not exist');
-    this.setRefreshTokenCookie(res, refreshedUserData.refreshToken);
-    res.send(this.mapUserDataToUserDto(refreshedUserData));
   }
 }
