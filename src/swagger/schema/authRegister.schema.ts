ff67d211674318533f640498190d22ea87428dcc
Тургумбаев Наиль Рашитович <nailsanti2k19@gmail.com>
2023-11-16 10:33:56 +0600

    #39 modified the documentation

--- src/swagger/schema/authRegister.schema.ts
+++ src/swagger/schema/authRegister.schema.ts
@@ -3,32 +3,52 @@ import { object, string, TypeOf } from 'zod';
 /**
  * @openapi
  * components:
- *  schemas:
- *    RegisterUserInput:
- *      type: object
- *      required:
- *        - email
- *        - phone
- *        - password
- *      properties:
- *        email:
- *          type: string
- *          default: jane.doe@example.com
- *        phone:
- *          type: string
- *          default: 87777777777
- *        password:
- *          type: string
- *          default: 123123
- *    RegisterUserResponse:
- *      type: object
- *      properties:
- *        email:
- *          type: string
- *        phone:
- *          type: string
- *        password:
- *          type: string
+ *   schemas:
+ *     AuthUserDto:
+ *       type: object
+ *       properties:
+ *         email:
+ *           type: string
+ *           format: email
+ *           example: jane.doe@example.com
+ *         phone:
+ *           type: string
+ *           pattern: '^\+?[1-9]\d{1,14}$'  # Adjust the pattern based on your phone number requirements
+ *           example: 87777777777
+ *         password:
+ *           type: string
+ *           example: 123123
+ *         role:
+ *           type: string
+ *           enum:
+ *             - patient
+ *             - psychologist
+ *           example: patient
+ *
+ *     RegisterUserInput:
+ *       type: object
+ *       required:
+ *         - password
+ *         - role
+ *       properties:
+ *         email:
+ *           $ref: '#/components/schemas/AuthUserDto/properties/email'
+ *         phone:
+ *           $ref: '#/components/schemas/AuthUserDto/properties/phone'
+ *         password:
+ *           $ref: '#/components/schemas/AuthUserDto/properties/password'
+ *         role:
+ *           $ref: '#/components/schemas/AuthUserDto/properties/role'
+ *
+ *     RegisterUserResponse:
+ *       type: object
+ *       properties:
+ *         email:
+ *           $ref: '#/components/schemas/AuthUserDto/properties/email'
+ *         phone:
+ *           $ref: '#/components/schemas/AuthUserDto/properties/phone'
+ *         password:
+ *           $ref: '#/components/schemas/AuthUserDto/properties/password'
  */
 
 export const registerUserSchema = object({
