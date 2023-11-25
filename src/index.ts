a13a67301910f6a71df877f99f8953ecabf2b2e6
Тургумбаев Наиль Рашитович <nailsanti2k19@gmail.com>
2023-11-25 21:13:33 +0600

    #53 shut down the psychologist's rota temporarily.

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,11 @@ import App from './app';
 import logger from './middlewares/logger.middleware';
 import { AuthRouter } from './routes/auth.route';
 import initCustomLocals from './middlewares/initCustomLocals.middleware';
-import { PsychologistRouter } from './routes/psychologist.route';
 
 const app = new App({
   port: 8000,
   middlewares: [initCustomLocals(), logger(), cookieParser(), cors()],
-  controllers: [new AuthRouter(), new PsychologistRouter()],
+  controllers: [new AuthRouter()],
 });
 
 app.listen();
