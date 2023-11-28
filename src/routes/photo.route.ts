0a0a518178b60d4efe64de75c0e2ba763745f026
kapusta02 <n.saratovtsev@mail.ru>
2023-11-28 14:35:24 +0600

    #63 created a router for adding photos

--- src/routes/photo.route.ts
+++ src/routes/photo.route.ts
@@ -1,27 +1,20 @@
-// import { Router } from 'express';
-// import { IRoute } from '../interfaces/IRoute.interface';
-// import { upload } from '../middlewares/ValidateUpload.middlewar';
-// import authenticateUser from '../middlewares/authenticateUser';
-// import { PhotoController } from '../controllers/photo.controller';
+import { Router } from 'express';
+import { IRoute } from '../interfaces/IRoute.interface';
+import { upload } from '../middlewares/ValidateUpload.middlewar';
+import authenticateUser from '../middlewares/authenticateUser';
+import { PhotoController } from '../controllers/photo.controller';
 
-// export class PhotoRouter implements IRoute {
-//   public path = '/photos';
-//   public router = Router();
-//   private controller: PhotoController;
+export class PhotoRouter implements IRoute {
+  public path = '/photos';
+  public router = Router();
+  private controller: PhotoController;
 
-//   constructor() {
-//     this.controller = new PhotoController();
-//     this.init();
-//   }
+  constructor() {
+    this.controller = new PhotoController();
+    this.init();
+  }
 
-//   private init() {
-//     this.router.post(
-//       '/create',
-//       authenticateUser,
-//       upload.fields([{ name: 'photo', maxCount: 1 }, { name: 'certificates' }]),
-//       this.controller.createPsychologistHandler,
-//     );
-//     this.router.get('/:id', this.controller.getOnePsychologistHandler);
-//     this.router.get('/', this.controller.getPsychologistsHandler);
-//   }
-// }
+  private init() {
+    this.router.post('/create', authenticateUser, upload.fields([{ name: 'photo', maxCount: 1 }]), this.controller.createPhoto);
+  }
+}
