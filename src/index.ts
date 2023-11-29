82fd53e367af49abc1cb9ac1fdf644daeabf23ce
Солтангазиева Мария Алексеевна <avemaryplus@gmail.com>
2023-11-29 20:38:33 +0600

    #64 fixed syntax error

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,6 @@ import { SymptomRouter } from './routes/symptom.route';
 import { TherapyMethodRouter } from './routes/therapyMethod.route';
 import { CertificateRouter } from './routes/certificate.route';
 import { PsychologistRouter } from './routes/psychologist.route';
-import { CertificateRouter } from './routes/certificate.route';
 
 const app = new App({
   port: 8000,
