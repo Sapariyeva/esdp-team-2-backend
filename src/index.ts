55c35ae567291c4ad3ef85cf01c3a237380bd942
Солтангазиева Мария Алексеевна <avemaryplus@gmail.com>
2023-11-30 16:20:51 +0600

    #54 fixed bugs

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,12 @@ import initCustomLocals from './middlewares/initCustomLocals.middleware';
 import { SymptomRouter } from './routes/symptom.route';
 import { PatientRouter } from './routes/patient.route';
 import { PsychologistRouter } from './routes/psychologist.route';
+import { TherapyMethodRouter } from './routes/therapyMethod.route';
 
 const app = new App({
   port: 8000,
   middlewares: [initCustomLocals(), logger(), cookieParser(), cors()],
-  controllers: [new PsychologistRouter(), new AuthRouter(), new SymptomRouter(), new TherapyMethodRouter(), new PatientRouter()]
+  controllers: [new PsychologistRouter(), new AuthRouter(), new SymptomRouter(), new TherapyMethodRouter(), new PatientRouter()],
 });
 
 app.listen();
