7a9033fc2ea781dca7d91c64fdc45369e6f33fc6
Dmitry <mikotsowl22@gmail.com>
2023-12-06 00:27:22 +0600

    #77 Add factories and seeds in TypeORM config

--- src/config/dataSource.ts
+++ src/config/dataSource.ts
@@ -8,6 +8,12 @@ import TherapyMethodSeeder from '../db/seeds/therapyMethod.seeds';
 import { TherapyMethodFactory } from '../db/factories/therapyMethod.factory';
 import TechniqueSeeder from '../db/seeds/technique.seeds';
 import { TechniqueFactory } from '../db/factories/technique.factory';
+import UserSeeder from '../db/seeds/user.seed';
+import { UserFactory } from '../db/factories/user.factory';
+import { PatientFactory } from '../db/factories/patient.factory';
+import PatientSeeder from '../db/seeds/patient.seeds';
+import PsychologistSeeder from '../db/seeds/psychologist.seeds';
+import { PsychologistFactory } from '../db/factories/psychologist.factory';
 
 const options: DataSourceOptions & SeederOptions = {
   type: 'mysql',
@@ -15,12 +21,12 @@ const options: DataSourceOptions & SeederOptions = {
   port: 3306,
   database: 'psyhelp_online',
   username: 'root',
-  password: '123trionix321',
+  password: 'admin',
   synchronize: true,
   logging: true,
   entities: ['src/entities/*.ts'],
-  seeds: [MainSeeder, SymptomsSeeder, TherapyMethodSeeder, TechniqueSeeder],
-  factories: [RoleFactory, SymptomsFactory, TherapyMethodFactory, TechniqueFactory],
+  seeds: [MainSeeder, SymptomsSeeder, TherapyMethodSeeder, TechniqueSeeder, UserSeeder, PsychologistSeeder, PatientSeeder],
+  factories: [RoleFactory, SymptomsFactory, TherapyMethodFactory, TechniqueFactory, UserFactory, PsychologistFactory, PatientFactory],
 };
 
 export const appDataSource = new DataSource(options);
