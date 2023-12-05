ddd4b7f538922254b8eb7f771de6fb0cd822e1e7
Dmitry <mikotsowl22@gmail.com>
2023-12-06 00:26:30 +0600

    #77 Add error trapping in executable seed file

--- src/db/init.seeds.ts
+++ src/db/init.seeds.ts
@@ -2,7 +2,13 @@ import { runSeeders } from 'typeorm-extension';
 import { appDataSource } from '../config/dataSource';
 
 appDataSource.initialize().then(async () => {
-  await appDataSource.synchronize(true);
-  await runSeeders(appDataSource);
+  try {
+    await appDataSource.synchronize(true);
+    await runSeeders(appDataSource);
+  } catch (e) {
+    if (e instanceof Error) console.error(e.message);
+    else console.error('Неизвестная ошибка');
+  }
+
   appDataSource.destroy();
 });
