7ad3c2497ea88936b4d76fa9f28d315bc61d8652
Dmitry <mikotsowl22@gmail.com>
2023-12-14 22:36:21 +0600

    #95 optimize creation users by role

--- src/db/seeds/user.seed.ts
+++ src/db/seeds/user.seed.ts
@@ -2,6 +2,7 @@ import { DataSource } from 'typeorm';
 import { Seeder, SeederFactoryManager } from 'typeorm-extension';
 import { User } from '../../entities/user.entity';
 import { Role } from '../../entities/role.entity';
+import { UserRole } from '../../interfaces/UserRole.enum';
 
 export default class UserSeeder implements Seeder {
   public async run(dataSource: DataSource, factoryManager: SeederFactoryManager): Promise<void> {
@@ -9,6 +10,14 @@ export default class UserSeeder implements Seeder {
     if (!roles.length) throw new Error('Не удалось получить роли для пользователей');
 
     const userFactory = factoryManager.get(User);
-    await Promise.all(roles.map(async (role) => await userFactory.saveMany(30, { roles: [role] })));
+    const userPromises: Promise<User[]>[] = roles.map(async (role) => {
+      let amount: number = 1;
+      if (role.name === UserRole.Psychologist) amount = 70;
+      if (role.name === UserRole.Patient) amount = 20;
+
+      return await userFactory.saveMany(amount, { roles: [role] });
+    });
+
+    await Promise.all(userPromises);
   }
 }
