d0f21913e758cf4f911502707f8d50ce9d3a2da7
Dmitry <mikotsowl22@gmail.com>
2023-12-17 02:35:10 +0600

    #106 Add token payload to all requests

--- src/middlewares/initCustomLocals.middleware.ts
+++ src/middlewares/initCustomLocals.middleware.ts
@@ -1,7 +1,22 @@
 import { RequestHandler } from 'express';
+import jwt from 'jsonwebtoken';
+import { IUserJwtPayload } from '../interfaces/IUser.interface';
+import config from '../config';
+
+const getTokenPayload = (accessToken: string | undefined): IUserJwtPayload | undefined => {
+  try {
+    if (!accessToken) throw new Error('Отсутствует токен');
+
+    return jwt.verify(accessToken, config.secretKey) as IUserJwtPayload;
+  } catch (e) {
+    return undefined;
+  }
+};
 
 const initCustomLocals = (): RequestHandler => (req, res, next) => {
-  req.customLocals = {};
+  req.customLocals = {
+    userJwtPayload: getTokenPayload(req.header('Authorization')),
+  };
 
   next();
 };
