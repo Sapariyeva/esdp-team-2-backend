c7ea0f1fa93977bfcda8ab72557c4942975416b7
Тургумбаев Наиль Рашитович <nailsanti2k19@gmail.com>
2024-01-22 16:38:09 +0600

    #188 the sum of one psychologist

--- src/routes/psychologist.route.ts
+++ src/routes/psychologist.route.ts
@@ -23,5 +23,6 @@ export class PsychologistRouter implements IRoute {
     this.router.delete('/:id', authenticateUser, checkUserRole('admin'), this.controller.deletePsychologistHandler);
     this.router.post('/filter', this.controller.filterPsychologists);
     this.router.get('/records/actual', authenticateUser, this.controller.getDateRecords);
+    this.router.get('/statistics/statistics', authenticateUser, this.controller.getSumByMonth);
   }
 }
